90793dab10469af80c1e927c8ab9523236284ffb
Hajin <hajin@Hajins-MacBook-Air.local>
2024-08-17 23:33:54 +0900

    feat: MessageBubbleOther 컴포넌트 구현 - Avatar 컴포넌트 추가

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,7 @@
+import './avatar.css';
+
+function Avatar() {
+  return <img src="src/assets/avatar.png" className="avatar" />;
+}
+
+export default Avatar;
--- src/components/MessageBubble/MessageBubbleOther.tsx
+++ src/components/MessageBubble/MessageBubbleOther.tsx
@@ -1,20 +1,20 @@
+import Avatar from '../Avatar/Avatar';
 import './messageBubble.css';
 
 interface MessageBubbleSelfProps {
   message: string;
   time: string;
-  isRead: boolean;
 }
 
-function MessageBubbleOther({ message, time, isRead }: MessageBubbleSelfProps) {
+function MessageBubbleOther({ message, time }: MessageBubbleSelfProps) {
   return (
     <div className="message-bubble-container">
+      <Avatar />
       <img src="src/assets/BubbleTipOther.svg" className="bubble-tip" />
       <div className="message-bubble">
         <p className="message-text">{message}</p>
         <div className="message-time">
           <p>{time}</p>
-          {isRead && <img src="src/assets/ReadCheck.svg" />}
         </div>
       </div>
     </div>
