a81009d3937030eae81f08ed13c2eb5ad312615b
Hajin <hajin@Hajins-MacBook-Air.local>
2024-08-21 00:08:49 +0900

    feat: 채팅방 스크롤 구현

--- src/components/MessageBubble/MessageBubbleOther.tsx
+++ src/components/MessageBubble/MessageBubbleOther.tsx
@@ -3,15 +3,17 @@ import './messageBubble.css';
 
 interface MessageBubbleSelfProps {
   message: string;
+  name: string;
   time: string;
 }
 
-function MessageBubbleOther({ message, time }: MessageBubbleSelfProps) {
+function MessageBubbleOther({ message, name, time }: MessageBubbleSelfProps) {
   return (
     <div className="message-bubble-container">
       <Avatar />
       <img src="src/assets/BubbleTipOther.svg" className="bubble-tip" />
       <div className="message-bubble">
+        <p className="sender-name">{name}</p>
         <p className="message-text">{message}</p>
         <div className="message-time">
           <p>{time}</p>
--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { socket } from '../../utils/socket';
 
 /* Components */
@@ -8,38 +8,125 @@ import MessageInput from '../../components/MessageInput/MessageInput';
 import Navbar from '../../components/Navbar';
 
 function Chat() {
+  // 채팅 스크롤
+  const chatRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
     socket.emit('joinRoom', {
       roomId: '66c045da1dbb6222e6c753f9',
       userId: localStorage.getItem('_id')!,
     });
+
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
   }, [socket]);
 
   return (
-    <div>
+    <div className="chat">
       <Navbar />
-      <div className="message-area">
-        <div className="message-area-self">
-          <MessageBubbleSelf
-            message={`저번에 갔던 곳 좋은 것 같아요.\n저번에 갔던 곳 좋은 것 같아요ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ.`}
-            time="오전 11:30"
-            isRead
-          />
-          <MessageBubbleSelf
-            message={`저번에 갔던 곳 좋은 것 같아요.\n저번에 갔던 곳 좋은 것 같아요ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ.`}
-            time="오전 11:30"
-            isRead
-          />
-        </div>
-        <div className="message-area-other">
-          <MessageBubbleOther
-            message={`저번에 갔던 곳 좋은 것 같아요.\n저번에 갔던 곳 좋은 것 같아요ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ.`}
-            time="오전 11:30"
-          />
-          <MessageBubbleOther
-            message={`저번에 갔던 곳 좋은 것 같아요.\n저번에 갔던 곳 좋은 것 같아요ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ.`}
-            time="오전 11:30"
-          />
+      <div className="message-container">
+        <div className="message-area" ref={chatRef}>
+          <div className="message-area-self">
+            <MessageBubbleSelf
+              message={`그 학교 옆에 그 카페 이름 뭐였죠?`}
+              time="오전 11:30"
+              isRead
+            />
+            <MessageBubbleSelf
+              message={`저번에 갔던 곳 좋은 것 같아요.`}
+              time="오전 11:30"
+              isRead
+            />
+          </div>
+          <div className="message-area-other">
+            <MessageBubbleOther
+              message={`오늘 회의 어디서 하나요?`}
+              name="원하진"
+              time="오전 11:30"
+            />
+          </div>
+          <div className="message-area-self">
+            <MessageBubbleSelf
+              message={`그 학교 옆에 그 카페 이름 뭐였죠?`}
+              time="오전 11:30"
+              isRead
+            />
+            <MessageBubbleSelf
+              message={`저번에 갔던 곳 좋은 것 같아요.`}
+              time="오전 11:30"
+              isRead
+            />
+          </div>
+          <div className="message-area-other">
+            <MessageBubbleOther
+              message={`오늘 회의 어디서 하나요?`}
+              name="원하진"
+              time="오전 11:30"
+            />
+          </div>
+          <div className="message-area-self">
+            <MessageBubbleSelf
+              message={`그 학교 옆에 그 카페 이름 뭐였죠?`}
+              time="오전 11:30"
+              isRead
+            />
+            <MessageBubbleSelf
+              message={`저번에 갔던 곳 좋은 것 같아요.`}
+              time="오전 11:30"
+              isRead
+            />
+          </div>
+          <div className="message-area-other">
+            <MessageBubbleOther
+              message={`오늘 회의 어디서 하나요?`}
+              name="원하진"
+              time="오전 11:30"
+            />
+            <MessageBubbleOther
+              message={`오늘 회의 어디서 하나요?`}
+              name="원하진"
+              time="오전 11:30"
+            />
+          </div>
+          <div className="message-area-self">
+            <MessageBubbleSelf
+              message={`그 학교 옆에 그 카페 이름 뭐였죠?`}
+              time="오전 11:30"
+              isRead
+            />
+            <MessageBubbleSelf
+              message={`저번에 갔던 곳 좋은 것 같아요.`}
+              time="오전 11:30"
+              isRead
+            />
+          </div>
+          <div className="message-area-other">
+            <MessageBubbleOther
+              message={`오늘 회의 어디서 하나요?`}
+              name="원하진"
+              time="오전 11:30"
+            />
+          </div>
+          <div className="message-area-self">
+            <MessageBubbleSelf
+              message={`그 학교 옆에 그 카페 이름 뭐였죠?`}
+              time="오전 11:30"
+              isRead
+            />
+            <MessageBubbleSelf
+              message={`저번에 갔던 곳 좋은 것 같아요.`}
+              time="오전 11:30"
+              isRead
+            />
+          </div>
+          <div className="message-area-other">
+            <MessageBubbleOther
+              message={`오늘 회의 어디서 하나요?`}
+              name="원하진"
+              time="오전 11:30"
+            />
+          </div>
         </div>
       </div>
       <MessageInput />
